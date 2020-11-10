c1d0ac6acf72d8ec6bc4410b14b5a4b805ac07d3
Sarah <70615195+smanter82@users.noreply.github.com>
2020-11-09 22:08:53 -0500

    fixed local storage and got last API call working

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
-let storedHistory = JSON.parse(localStorage.getItem("searchHistory")) || "[]"
-// if (storedHistory !== null) {
+//I need help with:  
+//How do I get the weather icons to pop up on forecast?
+//How do I get the for loop to work on the 5 day forecast?  (Or do I have to have each day populate separately?)
+//Am I using .toFixed() correctly?
+
+//Populate Search History on page loading
+let storedHistory = JSON.parse(localStorage.getItem("searchHistory"))
+if (storedHistory) {
 storedHistory.map(item => $("#cityList").append(`<li class="list-group-item">${item}</li>`))
-// } else {
-//     searchCity
-// }
+}
 
 
 $("#cityName").on("click", searchCity)
 
 function searchCity(){
     //set input variable and API query
-    
     let cityInput = $("#cityInput").val()
     var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=imperial&appid=2d589977b92f8d92891bc83cfda86d2c`;
+ 
+    let storedHistory = JSON.parse(localStorage.getItem("searchHistory"))
+    console.log(storedHistory)
     //set local storage for city search history list
     let array = []
+    // console.log(storedHistory)
+    if (storedHistory) {
     storedHistory.map(item => array.push(item))
     array.push(cityInput)
-    console.log(array)
-    console.log(storedHistory)
     localStorage.setItem("searchHistory", JSON.stringify(array))
+    } else {
+        console.log(cityInput)
+        array.push(cityInput)
+        localStorage.setItem("searchHistory", JSON.stringify(array))  
+        console.log(storedHistory)
+    }
     //Add searched cities to search history list
     $("#cityList").append(`<li class="list-group-item">${cityInput}</li>`)
 
@@ -77,30 +89,50 @@ function searchCity(){
                 $(`#uvI`).addClass("red");
             }
         })
+            //forecast URL is returning 401 (Unauthorized) error.
+            let forecastURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=2d589977b92f8d92891bc83cfda86d2c`
 
+            $.ajax({
+                url: forecastURL,
+                method: 'GET'
+            }).then(function(forecastData){
+                console.log(forecastData)
+                console.log(forecastData.daily[1])
+                let forecast = forecastData.daily
+                
+                
+
+            for (i=1; i<6; i++) {
+                console.log(forecast)
+                // let icon = forecast[i].weather[0].icon
+                // console.log(icon)
+                // $('img').append(icon)
+
+                let dailyTemp = forecast[i].temp.day
+                let dailyHumidity = forecast[i].humidity
+                console.log(dailyHumidity)
+                
+                $('.dailyTemp').append('Temp: ' + dailyTemp + `<br>` + 'Humidity: ' + dailyHumidity).tofixed(2);
+            
+            
+            }
     
+            })
+
+            //set 5-day forecast date variables.
+            let day1 = moment().add(1, 'days').calendar('MM/DD/YYYY')
+            let day2 = moment().add(2, 'days').calendar('MM/DD/YYYY')
+            let day3 = moment().add(3, 'days').calendar('MM/DD/YYYY')
+            let day4 = moment().add(4, 'days').calendar('MM/DD/YYYY')
+            let day5 = moment().add(5, 'days').calendar('MM/DD/YYYY')
+
+            //append 5-day forecast date variables to forecast containers.
+            $("#day1").append(" (" + day1 + ")")
+            $("#day2").append(" (" + day2 + ")")
+            $("#day3").append(" (" + day3 + ")")
+            $("#day4").append(" (" + day4 + ")")
+            $("#day5").append(" (" + day5 + ")")
+                            
+
     })
-
-    //forecast URL is returning 401 (Unauthorized) error.
-    // let forecastURL = `https://api.openweathermap.org/data/2.5/forecast/daily?q=${cityInput}&cnt=5&units=imperial&appid=2d589977b92f8d92891bc83cfda86d2c`
-    // $.ajax({
-    //     url: forecastURL,
-    //     method: 'GET'
-    // }).then(function(forecastData){
-    //     console.log(forecastData)
-    // //     console.log(forecastData.list[5])
-    // // let forecast = forecastData.list
-    // // for (i=0; i<forecastData.list.length; i++) {
-    // //     console.log(forecastData.list[i])
-    // })
-    // $("#uvI").append("UV Index: " + JSON.stringify(uvIndex))
-
-
-    
-    
 }
-// function searchPlants() {
-//     var plantName = $(this).attr("data-name");
-//     console.log(plantName)
-// }
-
