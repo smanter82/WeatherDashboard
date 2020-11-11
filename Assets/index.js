cc97bfc1a23c7e5618ce1516aff704f50b57020e
Sarah <70615195+smanter82@users.noreply.github.com>
2020-11-10 22:35:44 -0500

    Clear old data on click, working for loop and icons for 5 day, added commits

--- Assets/index.js
+++ Assets/index.js
@@ -1,148 +1,133 @@
-//I need help with:  
-//How do I get the weather icons to pop up on forecast?
-//How do I get the for loop to work on the 5 day forecast?  (Or do I have to have each day populate separately?)
-//Am I using .toFixed() correctly?
-//How do I get containers to empty on new search without making it keep from repopulating with the new data?
+//I still need to:
 
-//Don't forget to pull City search data back up on click of city in search history.
+//Icon for City Search Button
 
+//Get unix to come up as readable dates.
+    // $(document).ready(function() {
+    //   var value = $("#unixtime").val(); //this retrieves the unix timestamp
+    //   var dateString = moment(value).calendar(); 
+    //   alert(dateString);
+    // });
+
+
+const forecastDays = $("#forecastDays")
 //Populate Search History on page loading
-let storedHistory = JSON.parse(localStorage.getItem("searchHistory"))
+let storedHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
 if (storedHistory) {
-storedHistory.map(item => $("#cityList").append(`<li class="list-group-item">${item}</li>`))
+  storedHistory.map((item) =>
+    $("#cityList").prepend(`<li class="list-group-item cityBtn">${item}</li>`)
+  );
 }
+//Search for city weather on input button click.
+$("#cityName").on("click", function() {
+  const cityInput = $("#cityInput").val();
+  storedHistory.push(cityInput)
+  localStorage.setItem("searchHistory", JSON.stringify(storedHistory));
+  //Add searched cities to search history list
+  $("#cityList").prepend(`<li class="list-group-item cityBtn">${cityInput}</li>`);
+  searchCity(cityInput)
+});
+//Search for city data on click of city name from previous search in search history.
+$(document).on("click", ".cityBtn", function(){
+  console.log($(this).text())
+  searchCity($(this).text())
+})
 
+//Pull weather data from openweathermap.org
 
-$("#cityName").on("click", searchCity)
+function searchCity(cityInput) {
 
-function searchCity(){
-    //set input variable and API query
-    let cityInput = $("#cityInput").val()
-    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=imperial&appid=2d589977b92f8d92891bc83cfda86d2c`;
- 
-    let storedHistory = JSON.parse(localStorage.getItem("searchHistory"))
-    console.log(storedHistory)
-    //set local storage for city search history list
-    let array = []
-    // console.log(storedHistory)
-    if (storedHistory) {
-    storedHistory.map(item => array.push(item))
-    array.push(cityInput)
-    localStorage.setItem("searchHistory", JSON.stringify(array))
-    } else {
-        console.log(cityInput)
-        array.push(cityInput)
-        localStorage.setItem("searchHistory", JSON.stringify(array))  
-        console.log(storedHistory)
-    }
-    //Add searched cities to search history list
-    $("#cityList").append(`<li class="list-group-item">${cityInput}</li>`)
+  //set input variable and API query
+  const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=imperial&appid=2d589977b92f8d92891bc83cfda86d2c`;  
+
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    //Set today's date in today's weather container.
+    let m = moment().format("MM/DD/YYYY");
+    
+    //Set variables for API data
+    let location = response.name;
+    let temperature = response.main.temp;
+    let humidity = response.main.humidity;
+    let windSpeed = response.wind.speed;
+
+    // Clear data from previous searches in today's weather.
+    $("h1").empty();
+    $("#temp").empty();
+    $("#humidity").empty();
+    $("#wSpeed").empty();
+
+    // Populate today's weather info
+    $("h1").append(location + " (" + m + ")");
+    $("#temp").append("Temperature: " + JSON.stringify(temperature) + " °F");
+    $("#humidity").append("Humidity: " + JSON.stringify(humidity) + "%");
+    $("#wSpeed").append("Wind Speed: " + JSON.stringify(windSpeed) + "MPH");
+
+    //set latitude and longitude for UV Index ajax call.
+    let lat = response.coord.lat;
+    let lon = response.coord.lon;
+
+    //ajax call to get UV Index data from openweathermap.org
+    let uvURL = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=2d589977b92f8d92891bc83cfda86d2c`;
 
     $.ajax({
-        url:queryURL,
-        method: 'GET'
-    }).then(function(response){
-        
-        let m = moment().format('MM/DD/YYYY') 
-        //Set variables for API data
-        let location = response.name
-        let temperature = response.main.temp
-        let humidity = response.main.humidity
-        let windSpeed = response.wind.speed
+      url: uvURL,
+      method: "GET",
+    }).then(function (uvData) {
+      //set variable for UV Index data
+      let uvIndex = uvData.value;
+      //clear UV data from previous search.
+      $("#uvI").empty()
+      //Populate UV Index data to today's weather
+      $("#uvI").append("UV Index: " + JSON.stringify(uvIndex));
 
-        // $("#weatherToday").empty() <--Why does this make it so the box won't populate also on the button click.
+      //Set color change according to UV Index.
+      if (uvIndex < 3) {
+        $(`#uvI`).removeClass("white");
+        $(`#uvI`).removeClass("yellow");
+        $(`#uvI`).removeClass("red");
+        $(`#uvI`).addClass("green");
+      } else if (uvIndex >= 3 && uvIndex < 8) {
+        $(`#uvI`).removeClass("white");
+        $(`#uvI`).removeClass("green");
+        $(`#uvI`).removeClass("red");
+        $(`#uvI`).addClass("yellow");
+      } else {
+        $(`#uvI`).removeClass("white");
+        $(`#uvI`).removeClass("yellow");
+        $(`#uvI`).removeClass("green");
+        $(`#uvI`).addClass("red");
+      }
+    });
 
-        // Populate today's weather info
-        $("h1").append(location + " (" + m + ")")
-        $("#temp").append("Temperature: " + JSON.stringify(temperature) + " °F")
-        $("#humidity").append("Humidity: " + JSON.stringify(humidity) + "%")
-        $("#wSpeed").append("Wind Speed: " + JSON.stringify(windSpeed) + "MPH")
-        
-        // console.log(response)
+    //Ajax call for 5-day forecast from openweathermap.org
+    let forecastURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=2d589977b92f8d92891bc83cfda86d2c`;
 
-        //set latitude and longitude for UV Index ajax call.
-        let lat = response.coord.lat
-        let lon = response.coord.lon
+    $.ajax({
+      url: forecastURL,
+      method: "GET",
+    }).then(function (forecastData) {
+      let forecast = forecastData.daily;
 
-        //ajax call to get UV Index data from openweathermap.org
-        let uvURL = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=2d589977b92f8d92891bc83cfda86d2c`
-   
-        $.ajax({
-            url: uvURL,
-            method: 'GET'
-        }).then(function(uvData){
-            
-            //set variable for UV Index data
-            let uvIndex = uvData.value
-            //Populate UV Index data to today's weather
-            $("#uvI").append("UV Index: " + JSON.stringify(uvIndex))
-            
-            //Set color change according to UV Index.
-            if (uvIndex < 3) {
-                $(`#uvI`).removeClass("white");
-                $(`#uvI`).removeClass("yellow");
-                $(`#uvI`).removeClass("red");
-                $(`#uvI`).addClass("green");
-            } else if (uvIndex >= 3 && uvIndex < 8) {
-                $(`#uvI`).removeClass("white");
-                $(`#uvI`).removeClass("green");
-                $(`#uvI`).removeClass("red");
-                $(`#uvI`).addClass("yellow");
-            } else {
-                $(`#uvI`).removeClass("white");
-                $(`#uvI`).removeClass("yellow");
-                $(`#uvI`).removeClass("green");
-                $(`#uvI`).addClass("red");
-            }
-        })
-            //Ajax call for 5-day forecast from openweathermap.org
-            let forecastURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=2d589977b92f8d92891bc83cfda86d2c`
+      //Clear 5-day forecast data from previous search.
+      forecastDays.empty()
 
-            $.ajax({
-                url: forecastURL,
-                method: 'GET'
-            }).then(function(forecastData){
-                console.log(forecastData)
-                console.log(forecastData.daily[1])
-                let forecast = forecastData.daily
-                
-                
-
-            for (i=1; i<6; i++) {
-                console.log(forecast)
-
-                //Set icons for daily forecast
-
-                // let icon = forecast[i].weather[0].icon
-                // console.log(icon)
-                // $('img').append(icon)
-
-                let dailyTemp = forecast[i].temp.day
-                let dailyHumidity = forecast[i].humidity
-
-                
-                //Populate 5-day forecast with temperature and humidity.
-                $('.dailyTemp').append('Temp: ' + dailyTemp + `<br>` + 'Humidity: ' + dailyHumidity).tofixed(2);
-            
-            
-            }
-    
-            })
-
-            //set 5-day forecast date variables.
-            let day1 = moment().add(1, 'days').calendar('MM/DD/YYYY')
-            let day2 = moment().add(2, 'days').calendar('MM/DD/YYYY')
-            let day3 = moment().add(3, 'days').calendar('MM/DD/YYYY')
-            let day4 = moment().add(4, 'days').calendar('MM/DD/YYYY')
-            let day5 = moment().add(5, 'days').calendar('MM/DD/YYYY')
-
-            //append 5-day forecast date variables to forecast containers.
-            $("#day1").append(" (" + day1 + ")")
-            $("#day2").append(" (" + day2 + ")")
-            $("#day3").append(" (" + day3 + ")")
-            $("#day4").append(" (" + day4 + ")")
-            $("#day5").append(" (" + day5 + ")")
-                            
-
-    })
+      //Populate weather data for next 5 days.
+      for (i = 1; i < 6; i++) {
+        console.log("Creating Card", i);
+        const dayCard = 
+            `<div class="col">  
+             <div class="card-body days">
+               <h6 id="day1">${forecast[i].dt}</h6>
+               <img id="day1Icon" src = "https://openweathermap.org/img/w/${forecastData.daily[i].weather[0].icon}.png">
+               <p class="dailyTemp">${forecast[i].temp.day}</p>
+               <p class="dailyHumidity">${forecast[i].humidity}</p>
+             </div>
+             </div>`
+        forecastDays.append(dayCard)
+      }
+    });
+  });
 }
